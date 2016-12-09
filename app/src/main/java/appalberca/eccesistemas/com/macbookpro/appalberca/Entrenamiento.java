fd1fc61f717c2c4ddf0841d73426c96e542960bb
macbookpro <julio.martinezc@telmex.com>
2016-12-08 16:00:56 -0800

    Initial commit

---
+++ app/src/main/java/appalberca/eccesistemas/com/macbookpro/appalberca/Entrenamiento.java
@@ -0,0 +1,8 @@
+package appalberca.eccesistemas.com.macbookpro.appalberca;
+
+/**
+ * Created by macbookpro on 12/8/16.
+ */
+
+public class Entrenamiento {
+}
---
+++ app/src/main/java/appalberca/eccesistemas/com/macbookpro/appalberca/MainAlberca.java
@@ -0,0 +1,13 @@
+package appalberca.eccesistemas.com.macbookpro.appalberca;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainAlberca extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_alberca);
+    }
+}
